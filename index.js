d44ece4251335dde5d93c3b7a33832287a5a1455
boatengine <61369952+boatengine@users.noreply.github.com>
2022-07-29 20:58:38 +0700

    fix2

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.listen(PORT, () => {
     console.log(`App run on port ${PORT}`);
 })
 
-app.get('/', (req, res => {
+app.get('/', (req, res) => {
     res.send('This is my api');
-}))
+})
 
 app.get('/about', (req, res) => {
     res.send('this is my about route');
